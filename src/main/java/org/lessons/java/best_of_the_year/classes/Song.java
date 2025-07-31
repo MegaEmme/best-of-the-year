5119116210ad9b67c70561e0c06771ebb698a249
MegaEmme <mikmusco@gmail.com>
2025-07-31 11:20:40 +0200

    feat: create song class page and htlm implementation

--- src/main/java/org/lessons/java/best_of_the_year/classes/Song.java
+++ src/main/java/org/lessons/java/best_of_the_year/classes/Song.java
@@ -3,5 +3,35 @@ package org.lessons.java.best_of_the_year.classes;
 import java.io.Serializable;
 
 public class Song implements Serializable {
+    private int id;
+    private String title;
+
+    public Song() {
+    }
+
+    public Song(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String toString() {
+        return String.format("%d,%s", id, title);
+    }
 
 }
--- src/main/java/org/lessons/java/best_of_the_year/controller/HomeController.java
+++ src/main/java/org/lessons/java/best_of_the_year/controller/HomeController.java
@@ -3,10 +3,12 @@ package org.lessons.java.best_of_the_year.controller;
 import java.util.ArrayList;
 
 import org.lessons.java.best_of_the_year.classes.Movie;
+import org.lessons.java.best_of_the_year.classes.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -37,4 +39,25 @@ public class HomeController {
 
         return "movies";
     }
+
+    @GetMapping("/songs")
+    public String getBestSongs(Model model) {
+
+        Song canzoneUno = new Song(1, "La guerra di Piero");
+        Song canzoneDue = new Song(2, "Help");
+        Song canzoneTre = new Song(3, "Satisfaction");
+        Song canzoneQuattro = new Song(4, "Smoke on the Water");
+
+        ArrayList<Song> listaCanzoni = new ArrayList<>();
+
+        listaCanzoni.add(canzoneUno);
+        listaCanzoni.add(canzoneDue);
+        listaCanzoni.add(canzoneTre);
+        listaCanzoni.add(canzoneQuattro);
+
+        model.addAttribute("songs", listaCanzoni);
+
+        return "songs";
+    }
+
 }
